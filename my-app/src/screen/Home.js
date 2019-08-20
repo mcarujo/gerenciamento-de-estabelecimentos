622882bb169b8603c90261b2a24cab6804e3637f
Marco Carujo <marco.carujo@logicasistemas.com.br>
2019-08-20 11:24:34 -0300

    refatorando tela HOME

--- my-app/src/screen/Home.js
+++ my-app/src/screen/Home.js
@@ -17,23 +17,28 @@ export class Home extends Component {
           <div class="content">
             <h2 class="content-subhead">O projeto foi dividido em 3 partes.</h2>
             <p>
+              {" "}
+              {"- "}
               <a href="https://github.com/mcarujo/gerenciamento-de-estabelecimentos">
-                Master(Docker)
-              </a>
-              é o branch responsável por iniciar os dois micro serviços,
-              front-end e back-end. Dentro dele você vai encontrar dois
-              'Dockerfile' nos diretórios 'back-end' e 'front-end'. Ambos criam
-              o ambiente para execução dos seus respectivos serviços.
+                Master
+              </a>{" "}
+              (Docker) é o branch responsável por iniciar os dois micro
+              serviços, front-end e back-end. Dentro dele você vai encontrar
+              dois 'Dockerfile' nos diretórios 'back-end' e 'front-end'. Ambos
+              criam o ambiente para execução dos seus respectivos serviços.
             </p>
+
             <p>
+              {" "}
+              {"- "}
               <a href="https://github.com/mcarujo/gerenciamento-de-estabelecimentos/tree/back-end">
                 Back-end
-              </a>
+              </a>{" "}
               foi feito com o 'microframework'{" "}
-              <a href="https://flask.palletsprojects.com/en/1.1.x/">flask</a>
+              <a href="https://flask.palletsprojects.com/en/1.1.x/">flask</a>{" "}
               onde foi separado o 'model' do 'connector' utilizando os conceitos
-              dos{" "}
-              <a hred="https://www.eduardopires.net.br/2013/04/orientacao-a-objeto-solid/">
+              do{" "}
+              <a href="https://www.eduardopires.net.br/2013/04/orientacao-a-objeto-solid/">
                 SOLID
               </a>
               . A estrutura do banco de dados é feita pelo{" "}
@@ -46,9 +51,11 @@ export class Home extends Component {
               </a>
             </p>
             <p>
+              {" "}
+              {"- "}
               <a href="https://github.com/mcarujo/gerenciamento-de-estabelecimentos/tree/front-end">
                 Front-end
-              </a>
+              </a>{" "}
               feito em <a hred="https://reactjs.org/">React JS</a> onde
               basicamente foi construída quatro telas: Home, Form, Table e
               Login. Estas estão no diretório
