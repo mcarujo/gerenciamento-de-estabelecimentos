64f907b54ceadd270093ee888c78c31c865e688d
Marco Carujo <mamcarujo@gmail.com>
2019-08-20 22:19:22 -0300

    concatenando comando no start

--- back-end/start.sh
+++ back-end/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python3 migration.py db init
-python3 migration.py db migrate
-python3 migration.py db upgrade
-python3 seed.py
+python3 migration.py db init && \
+python3 migration.py db migrate && \
+python3 migration.py db upgrade && \
+python3 seed.py && \
 python3 app.py
